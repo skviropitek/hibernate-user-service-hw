d8d2975e93c228dc635c8ce0e9ea207c04bdf91f
skviropitek <skvyra.anton@gmail.com>
2025-07-09 23:06:42 +0200

    a

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,5 @@
 package mate.academy;
 
-import mate.academy.exception.AuthenticationException;
-import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Injector;
 import mate.academy.model.User;
 import mate.academy.service.AuthenticationService;
